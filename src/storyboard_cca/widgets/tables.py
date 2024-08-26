e89862ce01ef93ef19f941bf6b1009b1587013b8
Zachary Roy <zachary.roy@water.ca.gov>
2024-08-26 10:00:57 -0700

    Add separate table loading methods

---
+++ src/storyboard_cca/widgets/tables.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+from dash.dash_table import DataTable
+from pandas import read_csv
+
+assets = Path(__file__).parent.parent / "assets/text"
+
+
+def from_file(src: str | Path) -> DataTable:
+    if not isinstance(src, Path):
+        src = Path(src)
+    if src.suffix == "":
+        src = src.with_suffix(".csv")
+    if not src.exists():
+        src = assets / src
+    if not src.exists():
+        raise FileNotFoundError(src)
+    df = read_csv(src)
+    return DataTable(df.to_dict("records"), [{"name": i, "id": i} for i in df.columns])
