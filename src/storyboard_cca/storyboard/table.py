f8b57459d9acc7d0f42bfc552d506e416149e534
Zachary Roy <zachary.roy@water.ca.gov>
2024-08-29 09:47:04 -0700

    Add default styles for tables

--- src/storyboard_cca/storyboard/table.py
+++ src/storyboard_cca/storyboard/table.py
@@ -6,7 +6,7 @@ from pandas import read_csv
 assets = Path(__file__).parent.parent / "assets"
 
 
-def from_file(src: str | Path) -> DataTable:
+def from_file(src: str | Path, **kwargs) -> DataTable:
     if not isinstance(src, Path):
         src = Path(src)
     if src.suffix == "":
@@ -16,4 +16,16 @@ def from_file(src: str | Path) -> DataTable:
     if not src.exists():
         raise FileNotFoundError(src)
     df = read_csv(src)
-    return DataTable(df.to_dict("records"), [{"name": i, "id": i} for i in df.columns])
+    cca_kwargs = dict(
+        id=str(src),
+        style_data=dict(
+            whiteSpace="normal",
+            height="auto",
+            lineHeight="15px",
+        ),
+        style_table=dict(overflowX="auto"),
+        data=df.to_dict("records"),
+        columns=[{"name": i, "id": i} for i in df.columns],
+    )
+    cca_kwargs.update(kwargs)
+    return DataTable(**cca_kwargs)
