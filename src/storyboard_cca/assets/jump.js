2adbd786200c733365639279cda16a7e8f1d032e
Zachary Roy <zachary.roy@water.ca.gov>
2024-08-30 15:51:42 -0700

    Jump with delay to avoid slow reloads

--- src/storyboard_cca/assets/jump.js
+++ src/storyboard_cca/assets/jump.js
@@ -1,39 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     var hash = window.location.hash;
     
-    scrollToId = (id) => {
-        var ref = document.getElementById(id);
-        if (ref) {
-            setTimeout(function() {
-                console.debug("jumping to id: " + id)
-                ref.scrollIntoView({
-                    behavior: 'smooth', 
-                    block: 'start'
-                });
-            }, 100);
+    function scrollToHash() {
+        if (hash && document.getElementById(hash.substring(1))) {
+            document.getElementById(hash.substring(1)).scrollIntoView();
         }
     }
 
-    // Observe mutations to the body to detect when elements are added
-    var observer = new MutationObserver(
-        function(mutations, obs) {
-            if (document.getElementById(hash.substring(1))) {
-                scrollToId(hash.substring(1));
-                obs.disconnect(); // Stop observing after the element is found and scrolled to
-            };
-        }
-    );
+    // Add a small delay to ensure elements are rendered
+    setTimeout(scrollToHash, 1000); // Adjust the delay time (300 ms) as needed
 
-    if (hash) {
-        observer.observe(document.body, {
-            childList: true,
-            subtree: true
-        });
-    }
-
-    // Handle hash change events as well
+    // Listen for URL hash changes
     window.addEventListener('hashchange', function() {
         hash = window.location.hash;
-        scrollToId(hash.substring(1));
+        setTimeout(scrollToHash, 1000); // Delay scroll for hash change as well
     });
 });
