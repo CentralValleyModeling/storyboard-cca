d9210a6890f80718cf4ccf193d73018c22e60190
Zachary Roy <zachary.roy@water.ca.gov>
2024-09-18 08:13:27 -0700

    Add project page, and sankey diagrams

--- src/storyboard_cca/storyboard/plots/__init__.py
+++ src/storyboard_cca/storyboard/plots/__init__.py
@@ -3,3 +3,4 @@ from ._bar import average_annual_bar, wyt_bar
 from ._exceedance import annual_exceedance, exceedance
 from ._line import line
 from ._monthly import monthly
+from ._sankey import sankey
